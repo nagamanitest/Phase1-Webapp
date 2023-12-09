3eebb92198b0c00ce8a559405c28fbbd5095dabe
nagamani <nagamanibathala25@gmail.com>
2023-12-09 19:46:32 +0530

    web files

--- src/main/java/com/simplilearn/webapp/db/DatabaseConnection.java
+++ src/main/java/com/simplilearn/webapp/db/DatabaseConnection.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 /**
  * This class is for DatabaseConnection.
  * 
- * @author khanw
+ * @author nagamani
  */
 public class DatabaseConnection {
 
@@ -221,15 +221,15 @@ public class DatabaseConnection {
 			// e.printStackTrace();
 		} finally {
 			// clean up
-			try {
-				// step5 : close connection
-				rst.close();
-				statment.close();
-				connection.close();
-				System.out.println("5. Closing the connection.");
-			} catch (SQLException e) {
-				System.out.println("Exception Occured ::: " + e.getClass());
-			}
+//			try {
+//				// step5 : close connection
+//				rst.close();
+//				statment.close();
+//				connection.close();
+//				System.out.println("5. Closing the connection.");
+//			} catch (SQLException e) {
+//				System.out.println("Exception Occured ::: " + e.getClass());
+//			}
 
 		}
 
